247f91e98edb20b7b9a201a687153ffd9bd4b8db
Quazar <popbomb99@outlook.com>
2025-11-07 22:45:05 +0000

    Basic Player Movement

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    [SerializeField] float speed;
+    private float vertical;
+    private float horizontal;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Inputs();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.linearVelocity = new Vector2(horizontal * speed, vertical * speed);
+    }
+
+    private void Inputs()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+    }
+}
